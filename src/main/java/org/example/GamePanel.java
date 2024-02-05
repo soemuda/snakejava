6fd74649486ec26253f2cf69fd10489ba61dea45
Soemuda <madis.kask@omakase.ee>
2024-02-05 20:26:24 +0200

    Snake game java

---
+++ src/main/java/org/example/GamePanel.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int{}
+    GamePanel() {
+
+    }
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPresed(KeyEvent e) {
+
+        }
+    }
+    public void actionPreformed(ActionEvent e) {
+
+    }
+
+}
