05875211bfae54d68c0882e6539a213274a85443
Soemuda <madis.kask@omakase.ee>
2024-02-06 13:47:57 +0200

    Snake game java

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -12,11 +12,29 @@ public class GamePanel extends JPanel {
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
-    //final int x[] = new int{}
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
     GamePanel() {
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
     public void startGame(){
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
 
     }
 
@@ -25,6 +43,10 @@ public class GamePanel extends JPanel {
     }
 
     public void draw(Graphics g) {
+
+    }
+
+    public void newApple() {
 
     }
 
